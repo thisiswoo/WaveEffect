b52a86fe367ec100acf5503195c228bc2eae04fb
thisiswoo <thisiswoo0594@gmail.com>
2022-07-28 19:03:44 +0900

    img onload

--- app.js
+++ app.js
@@ -1,7 +1,74 @@
 
 class App {
     constructor() {
+        this.canvas = document.createElement('canvas');
+        document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext('2d');
 
+        this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.resize();
+
+        this.isLoaded = false;
+        this.imgPos = {
+            x: 0,
+            y: 0,
+            width: 0,
+            height: 0,
+        };
+
+        this.image = new Image();
+        this.image.src = 'worldmap_001.jpg';
+        this.image.onload = () => {
+          this.isLoaded = true;
+          this.drawImage();
+        };
+    }
+
+    resize() {
+        this.stageWidth = document.body.clientWidth;
+        this.stageHeight = document.body.clientHeight;
+
+        this.canvas.width = this.stageWidth * this.pixelRatio;
+        this.canvas.height = this.stageHeight * this.pixelRatio;
+        this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+        if (this.isLoaded) {
+            this.drawImage();
+        }
+    }
+
+    drawImage() {
+        const stageRatio = this.stageWidth / this.stageHeight;
+        const imgRatio = this.image.width / this.image.height;
+
+        this.imgPos.width = this.stageWidth;
+        this.imgPos.height = this.stageHeight;
+
+        if (imgRatio > stageRatio) {
+            this.imgPos.width = Math.round(
+                this.image.width * (this.stageHeight / this.image.height)
+            );
+            this.imgPos.x = Math.round(
+                (this.stageWidth - this.imgPos.width) / 2
+            );
+        } else {
+            this.imgPos.height = Math.round(
+                this.image.height * (this.stageWidth / this.image.width)
+            );
+            this.imgPos.y = Math.round(
+                (this.stageHeight - this.imgPos.height) / 2
+            );
+        }
+
+        this.ctx.drawImage(
+            this.image,
+            0, 0,
+            this.image.width, this.image.height,
+            this.imgPos.x, this.imgPos.y,
+            this.imgPos.width, this.imgPos.height,
+        );
     }
 }
 
