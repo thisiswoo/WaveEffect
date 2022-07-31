d056a8aed60180c2fb9c8c32b7c6dda6ace90662
thisiswoo <thisiswoo0594@gmail.com>
2022-07-31 14:38:09 +0900

    add click event

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import {Ripple} from "./ripple.js";
 
 class App {
     constructor() {
@@ -6,6 +7,8 @@ class App {
         this.ctx = this.canvas.getContext('2d');
 
         this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+        this.ripple = new Ripple();
 
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
@@ -24,6 +27,10 @@ class App {
           this.isLoaded = true;
           this.drawImage();
         };
+
+        window.requestAnimationFrame(this.animate.bind(this));
+
+        this.canvas.addEventListener('click', this.onClick.bind(this), false);
     }
 
     resize() {
@@ -33,6 +40,8 @@ class App {
         this.canvas.width = this.stageWidth * this.pixelRatio;
         this.canvas.height = this.stageHeight * this.pixelRatio;
         this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+        this.ripple.resize(this.stageWidth, this.stageHeight);
 
         if (this.isLoaded) {
             this.drawImage();
@@ -69,6 +78,26 @@ class App {
             this.imgPos.x, this.imgPos.y,
             this.imgPos.width, this.imgPos.height,
         );
+    }
+
+    animate() {
+        window.requestAnimationFrame(this.animate.bind(this));
+
+        this.ripple.animate(this.ctx);
+    }
+
+    onClick(e) {
+        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+        this.ctx.drawImage(
+            this.image,
+            0, 0,
+            this.image.width, this.image.height,
+            this.imgPos.x, this.imgPos.y,
+            this.imgPos.width, this.imgPos.height,
+        );
+
+        this.ripple.start(e.offsetX, e.offsetY);
     }
 }
 
